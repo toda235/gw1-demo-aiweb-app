d911b2cf42a30572f86b38770b590e42f5d3982c
NamoT9029 <238596706+NamoT9029@users.noreply.github.com>
2025-11-15 13:49:08 +0900

    調整

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 from transformers import pipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForTokenClassification
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from streamlit_option_menu import option_menu # インポート
 from streamlit_toggle import st_toggle_switch
@@ -17,7 +17,23 @@ def tokenizer_model():
     pipe = pipeline("text-classification", model="namopanda/deberta-v3-base-japanese-politeness", device=-1)
     return tokenizer, model, pipe
 
-
+st.set_page_config(page_title="メール文変換・評価", page_icon="✉")
+st.markdown("""
+    <style>
+    .stButton>button { 
+        background-color: #ff6b6b; 
+        color: white; 
+        border-radius: 10px; 
+        transition: transform 0.2s;
+        font-weight: 900;
+    }
+            
+    .stButton>button:hover { 
+        transform: scale(1.05); 
+        color: black;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 def build_prompt(user_query, inputs="", sep="\n\n### "):
     sys_msg = "以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす応答を書きなさい。"
@@ -31,12 +47,43 @@ def build_prompt(user_query, inputs="", sep="\n\n### "):
         p += sep + role + msg
     return p
 
+def build_prompt_funny(user_query, inputs="", sep="\n\n### "):
+    sys_msg = "以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす冗談交じりの陽気な応答を書きなさい。"
+    p = sys_msg
+    roles = ["指示", "応答"]
+    msgs = [": \n" + user_query, ": \n"]
+    if inputs:
+        roles.insert(1, "入力")
+        msgs.insert(1, ": \n" + inputs)
+    for role, msg in zip(roles, msgs):
+        p += sep + role + msg
+    return p
+
+def readonly_textarea(title, text, min_height="1em"):
+    if title != "":
+        st.write(f"### {title}")
+    st.markdown(f"""
+    <div style="
+        width: 100%;
+        background-color:#f0f0f0;
+        padding:15px;
+        border-radius:10px;
+        font-family: sans-serif;
+        font-size:16px;
+        color:black;
+        white-space: pre-wrap;
+        word-wrap: break-word;
+        margin-bottom:20px;
+        min-height: {min_height};
+    ">{text}</div>
+    """, unsafe_allow_html=True)
+
+
+#準備
 tokenizer, model, pipe = tokenizer_model()
 if torch.cuda.is_available():
     model = model.to("cuda")
-
-# client = InferenceClient(api_key="")
-
+client = InferenceClient(api_key="")
 labels = {"LABEL_0":3, "LABEL_1":2, "LABEL_2":1, "LABEL_3":0}
 
 
@@ -74,13 +121,13 @@ if selected_mode == "メール文章変換":
     )
 
     status = st_toggle_switch(
-        label="件名も作成　",
+        label="件名生成　",
         key="switch_1",
         default_value=False,
         label_after=False, 
         inactive_color="#D3D3D3",  
-        active_color="#11567f",  
-        track_color="#29B5E8"  
+        active_color= "#fd0000",  
+        track_color="#ff6b6b"  
     )
 
     if selected_method == "普通モード":
@@ -107,35 +154,145 @@ if selected_mode == "メール文章変換":
 
     # 入力欄
     text = st.text_area("テキストを入力", "ごめん、明日の会議だけど、資料がまだできてないからリスケでお願い。")
-    user_inputs = {
-        "user_query": "与えられた文章を、ビジネスメール風の丁寧で簡潔な文章に変換してください。",
-        "inputs": text
-    }
-    prompt = build_prompt(**user_inputs)
-
-
 
     if st.button("変換する"):
         with st.spinner("変換中..."):
-            with torch.no_grad():
-                input_ids = tokenizer.encode(
-                    prompt, 
-                    add_special_tokens=True, 
-                    return_tensors="pt"
+            st.divider()
+            if selected_method == "普通モード":
+                with torch.no_grad():
+                    if status:
+                        user_inputs_title = {
+                            "user_query": "入力される文章の内容に合う適切な件名を出力してください。",
+                            "inputs": text
+                        }
+                        prompt_title = build_prompt(**user_inputs_title)
+                        input_ids_title = tokenizer.encode(
+                            prompt_title, 
+                            add_special_tokens=True, 
+                            return_tensors="pt"
+                        )
+
+                        tokens_title = model.generate(
+                            input_ids_title.to(device=model.device),
+                            max_new_tokens=256,
+                            temperature=0.5,
+                            top_p=0.95,
+                            do_sample=True,
+                        )
+
+                        out_title = tokenizer.decode(tokens_title[0][input_ids_title.shape[1]:], skip_special_tokens=True).strip()
+                        readonly_textarea("件名", out_title, min_height="3em")
+
+                    user_inputs = {
+                        "user_query": "与えられた文章を、ビジネスメール風の丁寧で簡潔な文章に変換してください。",
+                        "inputs": text
+                    }
+                    prompt = build_prompt(**user_inputs)
+                    input_ids = tokenizer.encode(
+                        prompt, 
+                        add_special_tokens=True, 
+                        return_tensors="pt"
+                    )
+
+                    tokens = model.generate(
+                        input_ids.to(device=model.device),
+                        max_new_tokens=512,
+                        temperature=0.5,
+                        top_p=0.95,
+                        do_sample=True,
+                    )
+
+                    out = tokenizer.decode(tokens[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
+                readonly_textarea("変換文", out)
+            
+            elif selected_method == "高級モード":
+                if status:
+                    messages_title = [
+                        {
+                            "role": "user",
+                            "content": f"""
+                    [SYSTEM]
+                    入力された文章に対する適切な件名を出力してください。
+                    
+                    [USER]
+                    {text}
+                    """
+                        }
+                    ]
+
+                    completion_title = client.chat.completions.create(
+                        model="openai/gpt-oss-120b", 
+                        messages=messages_title, 
+                        max_tokens=256
+                    )
+                    readonly_textarea("変換文", completion_title.choices[0].message.content)
+
+                messages = [
+                    {
+                        "role": "user",
+                        "content": f"""
+                [SYSTEM]
+                入力された文章を、簡潔なビジネスメール風のに変換してください。件名や署名は不要です。本文のみを出力してください。
+                
+                [USER]
+                {text}
+                """
+                    }
+                ]
+
+                completion = client.chat.completions.create(
+                    model="openai/gpt-oss-120b", 
+                    messages=messages, 
+                    max_tokens=512
                 )
+                readonly_textarea("変換文", completion.choices[0].message.content)
 
-                tokens = model.generate(
-                    input_ids.to(device=model.device),
-                    max_new_tokens=256,
-                    temperature=1,
-                    top_p=0.95,
-                    do_sample=True,
-                )
+            elif selected_method == "冗談モード":
+                with torch.no_grad():
+                    if status:
+                        user_inputs_title = {
+                            "user_query": "入力される文章の内容をフレンドリーで簡潔な件名に変換してください。",
+                            "inputs": text
+                        }
+                        prompt_title = build_prompt_funny(**user_inputs_title)
+                        input_ids_title = tokenizer.encode(
+                            prompt_title, 
+                            add_special_tokens=True, 
+                            return_tensors="pt"
+                        )
 
-                out = tokenizer.decode(tokens[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
-            st.success(out)
-            print(out)
+                        tokens_title = model.generate(
+                            input_ids_title.to(device=model.device),
+                            max_new_tokens=256,
+                            temperature=0.7,
+                            top_p=0.95,
+                            do_sample=True,
+                        )
 
+                        out_title = tokenizer.decode(tokens_title[0][input_ids_title.shape[1]:], skip_special_tokens=True).strip()
+                        readonly_textarea("件名", out_title)
+
+                    user_inputs = {
+                        "user_query": "入力される文章をフレンドリーで簡潔な文章に変換してください。",
+                        "inputs": text
+                    }
+                    prompt = build_prompt_funny(**user_inputs)
+                    input_ids = tokenizer.encode(
+                        prompt, 
+                        add_special_tokens=True, 
+                        return_tensors="pt"
+                    )
+
+                    tokens = model.generate(
+                        input_ids.to(device=model.device),
+                        max_new_tokens=512,
+                        temperature=0.7,
+                        top_p=0.95,
+                        do_sample=True,
+                    )
+
+                    out = tokenizer.decode(tokens[0][input_ids.shape[1]:], skip_special_tokens=True).strip()
+                    readonly_textarea("変換文", out)
 
 elif selected_mode == "メール評価":
     selected_method = option_menu(
@@ -159,11 +316,11 @@ elif selected_mode == "メール評価":
     text = st.text_area("テキストを入力", "ごめん、明日の会議だけど、資料がまだできてないからリスケでお願い。")
     if st.button("評価する"):
         with st.spinner("評価中..."):
+            st.divider()
             if selected_method == "丁寧レベル評価":
                 output = pipe(text)
                 level = labels[output[0]["label"]]
                 score = int(output[0]["score"]*100)
-
                 st.markdown(
                     f"""
                     <div style="
@@ -181,37 +338,23 @@ elif selected_mode == "メール評価":
                     unsafe_allow_html=True
                 )
             elif selected_method == "メール添削評価":
-                pass
-                # messages = [
-                #     {
-                #     "role": "user",
-                #     "content": "hello"
-                #     }
-                # ]
+                messages = [
+                    {
+                        "role": "user",
+                        "content": f"""
+                [SYSTEM]
+                入力された文章がビジネスメールとして適切か評価してください。ポイントに分けて添削してください。署名は考慮しないでください。
+                
+                [USER]
+                {text}
+                """
+                    }
+                ]
 
-                # completion = client.chat.completions.create(
-                #     model="meta-llama/Llama-3.2-3B-Instruct", 
-                #     messages=messages, 
-                #     max_tokens=500
-                # )
+                completion = client.chat.completions.create(
+                    model="openai/gpt-oss-120b", 
+                    messages=messages, 
+                    max_tokens=1024
+                )
 
-                # print(completion.choices[0].message.content)
-
-
-    
-
-    # client = InferenceClient(api_key="")
-    # messages = [
-    #     {
-    #     "role": "user",
-    #     "content": "hello"
-    #     }
-    # ]
-
-    # completion = client.chat.completions.create(
-    #     model="meta-llama/Llama-3.2-3B-Instruct", 
-    #     messages=messages, 
-    #     max_tokens=500
-    # )
-
-    # print(completion.choices[0].message.content)
+                st.write(completion.choices[0].message.content)
